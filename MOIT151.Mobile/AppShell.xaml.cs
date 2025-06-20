a3b83d8fdaddb088eaec20156ddefb4207a5e3b0
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2025-06-21 00:59:54 +0800

    chore: create mobile project

---
+++ MOIT151.Mobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MOIT151.Mobile;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MOIT151.Mobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MOIT151.Mobile;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
