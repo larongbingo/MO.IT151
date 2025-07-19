ff1cfe76798b35e3b54a6a27e5b20a5d23aed429
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2025-07-20 02:10:14 +0800

    feat: complete all required apis

--- server/src/main/kotlin/noobnoob/mmdc/Storage.kt
+++ server/src/main/kotlin/noobnoob/mmdc/Storage.kt
@@ -3,7 +3,9 @@ package noobnoob.mmdc
 import aws.sdk.kotlin.runtime.auth.credentials.StaticCredentialsProvider
 import aws.sdk.kotlin.services.s3.S3Client
 import aws.sdk.kotlin.services.s3.model.GetObjectRequest
+import aws.sdk.kotlin.services.s3.model.HeadObjectRequest
 import aws.sdk.kotlin.services.s3.model.PutObjectRequest
+import aws.sdk.kotlin.services.s3.presigners.presignGetObject
 import aws.sdk.kotlin.services.s3.presigners.presignPutObject
 import aws.smithy.kotlin.runtime.auth.awscredentials.Credentials
 import aws.smithy.kotlin.runtime.net.url.Url
@@ -21,7 +23,7 @@ private val client = S3Client {
 private val BUCKET_NAME = System.getenv("MOIT151_S3_BUCKET_NAME")
 
 class Storage(val s3Client: S3Client = client, val bucketName: String = BUCKET_NAME) : StorageService {
-    override suspend fun getPresignedUrl(fileName: String): String? {
+    override suspend fun getUploadPresignedUrl(fileName: String): String? {
         val unsignedRequest = PutObjectRequest {
             bucket = bucketName
             key = fileName
@@ -31,8 +33,33 @@ class Storage(val s3Client: S3Client = client, val bucketName: String = BUCKET_N
 
         return presignedRequest.url.toString()
     }
+    override suspend fun objectExistsByKey(fileName: String): Boolean {
+        val request = HeadObjectRequest {
+            bucket = bucketName
+            key = fileName
+        }
+
+        try {
+            s3Client.headObject(request)
+            return true
+        } catch(e: Exception) {
+            return false
+        }
+    }
+    override suspend fun getDownloadPresignedUrl(fileName: String): String? {
+        val downloadRequest = GetObjectRequest {
+            bucket = bucketName
+            key = fileName
+        }
+
+        val presignedRequest = s3Client.presignGetObject(downloadRequest, 1.hours)
+
+        return presignedRequest.url.toString()
+    }
 }
 
 interface StorageService {
-    suspend fun getPresignedUrl(fileName: String): String?
+    suspend fun getUploadPresignedUrl(fileName: String): String?
+    suspend fun objectExistsByKey(fileName: String): Boolean
+    suspend fun getDownloadPresignedUrl(fileName: String): String?
 }
