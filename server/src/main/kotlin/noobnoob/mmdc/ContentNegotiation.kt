b4dfad2350589cdc08444f26a2d68a866765b9df
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2025-07-19 19:47:13 +0800

    fix: incorrect serialization of Instant

--- server/src/main/kotlin/noobnoob/mmdc/ContentNegotiation.kt
+++ server/src/main/kotlin/noobnoob/mmdc/ContentNegotiation.kt
@@ -7,36 +7,38 @@ import java.util.UUID
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
-@OptIn(ExperimentalUuidApi::class)
+@OptIn(ExperimentalUuidApi::class, ExperimentalTime::class)
 fun Application.configureContentNegotiation() {
     install(ContentNegotiation) {
         gson {
             registerTypeAdapter(UUID::class.java, JavaUuidDAdapter())
             registerTypeAdapter(Uuid::class.java, KotlinUuidDAdapter())
+            registerTypeAdapter(Instant::class.java, KotlinInstantAdapter())
         }
     }
 }
 
-private class JavaUuidDAdapter : JsonSerializer<UUID>, JsonDeserializer<UUID> {
+private class JavaUuidDAdapter : JsonSerializer<UUID> {
     override fun serialize(src: UUID, typeOfSrc: Type, context: JsonSerializationContext): JsonElement {
         return JsonPrimitive(src.toString())
-    }
-
-    override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): UUID {
-        return UUID.fromString(json.asString)
     }
 }
 
 @OptIn(ExperimentalUuidApi::class)
-private class KotlinUuidDAdapter : JsonSerializer<Uuid>, JsonDeserializer<Uuid> {
+private class KotlinUuidDAdapter : JsonSerializer<Uuid> {
     override fun serialize(src: Uuid, typeOfSrc: Type, context: JsonSerializationContext): JsonElement {
         return JsonPrimitive(src.toString())
     }
+}
 
-    override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): Uuid? {
-        return Uuid.parse(json!!.asString)
+@OptIn(ExperimentalTime::class)
+private class KotlinInstantAdapter : JsonSerializer<Instant> {
+    override fun serialize(src: Instant?, typeOfSrc: Type?, context: JsonSerializationContext?): JsonElement? {
+        return JsonPrimitive(src.toString())
     }
 }
